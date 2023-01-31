8db7aec4d2331d519c8de3c148bec2877990a932
Mark Arciaga <mark.arciaga@adadevelopersacademy.org>
2023-01-31 14:37:10 -0800

    add gcp auth, docker auth, build and push to artifact registry

--- tests/index.test.js
+++ tests/index.test.js
@@ -8,7 +8,7 @@ describe("/cats endpoint", () => {
   test("It should respond with a 200 when making a GET", async () => {
     const response = await request(app).get("/cats");
 
-    expect(response.statusCode).toBe(400);
+    expect(response.statusCode).toBe(200);
     expect(response.body.cats).toStrictEqual(cats);
   });
 });
